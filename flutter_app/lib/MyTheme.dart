a496ba49580eae3f00b5a11eaa8e846fd6d8a275
cheyx <768095023@qq.com>
2019-08-28 23:24:09 +0800

    Please enter theommit message for your changes. Lines starting

--- flutter_app/lib/MyTheme.dart
+++ flutter_app/lib/MyTheme.dart
@@ -24,7 +24,6 @@ class MyHomePage extends StatelessWidget {
   final String title;
 
   MyHomePage({Key key, @required this.title}) :super(key: key)
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
-void main() => runApp(new MyApp());
+import 'moudle/MyMoudle.dart';
+
+//void main() => runApp(new MyContain());
+
+//void main()=>runApp(new ImageDemo());
+
+//void main()=>runApp(
+//  new MaterialApp(
+//    title: 'A',
+//    home: new ContainerDemo(),
+//  )
+//);
+
+
+//void main() =>
+//    runApp(
+//        new MaterialApp(
+//          title: 'b',
+//          home: new LayoutDemo(),
+//        ),
+//    );
+
+
+void main()=>runApp(new MyRaisedButton());
 
 
   
