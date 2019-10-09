86fafd353f86778d441b53dced63e2bb8cb2d1ad
cheyx <768095023@qq.com>
2019-10-09 20:10:01 +0800

    update

---
+++ hello_shecare/lib/widgets/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('AA'),
+      ),
+    );
+  }
+}
