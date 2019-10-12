c3aa46533d5be24f494534b5afc1ed7a43fc51da
cheyx <768095023@qq.com>
2019-10-12 19:05:41 +0800

    ios

--- flutter_shecare/lib/widgets/shop.dart
+++ flutter_shecare/lib/widgets/shop.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 final shopUrl = 'https://wechat.shecarefertility.com/dist/index.html?';
 
 class Shop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        "/": (_) => new WebviewScaffold(
-              url: shopUrl,
-            ),
-      },
-
-
+    return Scaffold(
+      body: Center(
+        child: Text('shop'),
+      ),
     );
   }
 }
